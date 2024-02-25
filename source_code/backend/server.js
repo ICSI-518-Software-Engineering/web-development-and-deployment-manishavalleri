e0db7778291ae88d79f89d7bee854fc14b9e0058
manishavalleri <mvalleri@albany.edu>
2024-02-25 12:59:36 -0500

    changes

--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
