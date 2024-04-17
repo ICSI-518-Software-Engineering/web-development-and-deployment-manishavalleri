43b6a7215b2f55eb0978be5a76a6bb085c93b247
manishavalleri <mvalleri@albany.edu>
2024-04-17 13:53:33 -0400

    package update

--- source_code/frontend/src/App.js
+++ source_code/frontend/src/App.js
@@ -5,6 +5,10 @@ import Addition from '../src/COMPONENTS/Addition';
 import Navbar from '../src/COMPONENTS/navbar';
 import Card from './COMPONENTS/card';
 import InventoryPage from './COMPONENTS/Inventory';
+import SignUp from './COMPONENTS/SignUp';
+import Login from './COMPONENTS/Login';
+import UserInfo from './COMPONENTS/userInfo';
+import Home from './COMPONENTS/Home';
 
 const App = () => {
   return (
@@ -13,12 +17,17 @@ const App = () => {
         <Navbar />
         <Routes>
             
-          <Route path="/" element={<NameInput />} />
+          <Route path="/" element={<Home />} />
         
           <Route path="/addition" element={<Addition />} />
           <Route path="/profile" element={<NameInput />} />
           <Route path="/third-party" element={<Card />} />
           <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<UserInfo />} />
+          
+          
           
           
         </Routes>
